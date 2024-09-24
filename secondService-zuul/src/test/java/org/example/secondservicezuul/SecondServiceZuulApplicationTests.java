e6b162393ab1e4d9a179daaa0537d88c46476812
IMWoo94 <leemeawoo94@gmail.com>
2024-09-24 20:37:53 +0900

    init : Spring Boot 2.4.5 / Zuul Test project Set
    1. Netflix Zuul Gateway 구현을 위한 init
    - Spring Boot 2.4.5
    - Spring Cloud 2020.0.2
    - Gradle 7.2
    - Java 11

---
+++ secondService-zuul/src/test/java/org/example/secondservicezuul/SecondServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.secondservicezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
