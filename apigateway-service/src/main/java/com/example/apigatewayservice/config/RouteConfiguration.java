b738cabfd6890f451935a52c8a729f03d7a99cd9
IMWoo94 <leemeawoo94@gmail.com>
2024-09-26 17:18:55 +0900

    fix : 비동기 Gateway 로 선언하면서 route 수정
    1. route 등록 변경

--- apigateway-service/src/main/java/com/example/apigatewayservice/config/RouteConfiguration.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/config/RouteConfiguration.java
@@ -1,21 +1,18 @@
 package com.example.apigatewayservice.config;
 
-import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.*;
-import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.*;
-import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.*;
-
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
 
 @Configuration
 public class RouteConfiguration {
 
 	@Bean
-	public RouterFunction<ServerResponse> routes() {
-		return route("second-service")
-			.route(path("/second-service/**"), http("http://localhost:8082"))
+	public RouteLocator getewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(r -> r.path("/second-service/**")
+				.uri("http://localhost:8082"))
 			.build();
 	}
 }
