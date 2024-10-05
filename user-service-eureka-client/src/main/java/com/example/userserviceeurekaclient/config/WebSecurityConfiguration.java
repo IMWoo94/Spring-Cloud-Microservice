d20029a8fe24c72067f5c7d55152fed7418e6314
IMWoo94 <leemeawoo94@gmail.com>
2024-10-05 17:40:23 +0900

    feat : Spring Security 적용
    1. Spring Security 의존성 추가
    - "/users/**", "/health_check", "/welcome", h2 console .. 일부 url 의 경우 permitAll() 처리 단, securityFilter 를 타지 않는 것은 아니므로 인지할 것
    2. csrf 검증 비활성화
    3. h2 console 의 iframe 화면 구성을 위해 X-Frame-Options 수정
    - DENY -> SAMEORIGIN
    4. h2 console 의 경우 개발 환경에서만 구동될 가능성이 크기에 Security 자체 무시 처리
    - properties 정보로 "spring.h2.console.enable" 가 "true" 를 가지면 무시 처리

---
+++ user-service-eureka-client/src/main/java/com/example/userserviceeurekaclient/config/WebSecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.example.userserviceeurekaclient.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// 모든 요청에 대해서 인증 처리를 진행 할 것이다.
+		http.authorizeHttpRequests((request) ->
+				request
+					.requestMatchers("/users/**", "/health_check", "/welcome")
+					.permitAll()
+					.requestMatchers(PathRequest.toH2Console())
+					.permitAll()
+			)
+			// h2 웹 콘솔 iframe 화면 구성 허용
+			// default : HeaderWriterFilter -> X-Frame-Options DENY
+			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+			.cors(Customizer.withDefaults())
+			// REST API 를 개발함으로 Session 관련 생성되지 않도록 처리
+			.sessionManagement(
+				(session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			// csrf 검증은 제외
+			.csrf(CsrfConfigurer::disable)
+			// 기본 인증 방식을 사용
+			.httpBasic(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	@ConditionalOnProperty(name = "spring.h2.console.enable", havingValue = "true")
+	// h2 console spring security 통과 하지 않도록 무시
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return webSecurity ->
+			webSecurity
+				.ignoring()
+				.requestMatchers(PathRequest.toH2Console());
+	}
+}
