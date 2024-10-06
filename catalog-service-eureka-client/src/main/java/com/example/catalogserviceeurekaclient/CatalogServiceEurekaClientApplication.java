ee5be48b85736f213947478fce24cf581f26694a
IMWoo94 <leemeawoo94@gmail.com>
2024-10-06 14:09:43 +0900

    init : Catalog Microservice Init
    1. Catalog 프로젝트 Set
    - Spring web / JPA / H2

---
+++ catalog-service-eureka-client/src/main/java/com/example/catalogserviceeurekaclient/CatalogServiceEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogserviceeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceEurekaClientApplication.class, args);
+	}
+
+}
