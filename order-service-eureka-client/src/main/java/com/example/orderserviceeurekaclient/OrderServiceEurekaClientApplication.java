3992ab942627b8b5ce2a80d3369b6a1941831b58
IMWoo94 <leemeawoo94@gmail.com>
2024-10-28 20:48:56 +0900

    init : Order Microservice init

---
+++ order-service-eureka-client/src/main/java/com/example/orderserviceeurekaclient/OrderServiceEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderserviceeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceEurekaClientApplication.class, args);
+	}
+
+}
---
+++ order-service-eureka-client/src/test/java/com/example/orderserviceeurekaclient/OrderServiceEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderserviceeurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceEurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
